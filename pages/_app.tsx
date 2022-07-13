ca0cbb8ed63d9c93229d36873096caaf620576ec
Michael Stubbs <michael.stubbs@live.co.uk>
2022-07-13 16:13:55 +0100

    Created new variables to update upon authentication of user once firebase auth is built in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
 import '../styles/styles.scss'
 import type { AppProps } from 'next/app'
 import Layout from '../layouts/Layout'
+import {AppContext} from '../context/context';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  
+  const [isAuthenticated, setIsAuthenticated ] = useState<boolean>(false);
+
   return(
-    <Layout>
-      <Component {...pageProps} />
-    </Layout> 
+    <AppContext.Provider value={{
+        isAuthenticated, setIsAuthenticated
+    }}>
+        <Layout>
+        <Component {...pageProps} />
+        </Layout> 
+    </AppContext.Provider>
   )
 }
 

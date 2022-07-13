6f1fdd69ab9bd2d848741f5b8e59017e448567b0
Michael Stubbs <michael.stubbs@live.co.uk>
2022-07-13 16:12:53 +0100

    Created loginsdebar component for decorative piece on login page

---
+++ modules/LoginSidebar.tsx
@@ -0,0 +1,21 @@
+import { Col, Row } from "react-bootstrap";
+
+const LoginSidebar = () => {
+    return (
+       <div className="sidebar-right">
+            <Row className="h-100">
+                {/* <Col className="d-flex col-12 justify-content-center align-items-center h-25">
+                    <div className="avatar">
+
+                    </div>
+                </Col> */}
+                <Col className="col-12 d-flex justify-content-center">
+                    Hello
+                </Col>
+            </Row>
+       </div>
+    
+    )
+}
+
+export default LoginSidebar;
